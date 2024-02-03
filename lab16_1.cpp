a96bd9e27e498af36ada715f035ba334f6204d13
Bhumipat <bhumipat2004@gmail.com>
2024-02-03 10:39:22 +0700

    completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,40 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double* p, int N, int M){
+    cout << fixed << setprecision(2);
+    for (int i = 0; i < N*M; i++){
+        cout << *(p+i);
+        if (i % M == M - 1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double* p, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(p+i) = (rand() % 101) * 0.01;
+    }
+}
+
+void findRowSum(const double* p, double* b, int N, int M) {
+    double sum1 = 0;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            sum1 += *(p+i*M+j);
+        }
+        *(b+i) = sum1;
+        sum1 = 0;
+    }
+}
+
+void findColSum(const double* p, double* b, int N, int M) {
+    double sum2 = 0;
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < N; j++){
+            sum2 += *(p+i+j*M);
+        }
+        *(b+i) = sum2;
+        sum2 = 0;
+    }
+}
